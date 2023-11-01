a648163ca445b6a01b6cae9f17f4f579d0076a7f
E-Kat101 <katrom567@gmail.com>
2023-11-01 12:56:55 -0400

    Started Scrolling Background Code

---
+++ Assets/BGScroll.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+
+    public float ScrollSpeed = 2f;
+    private Vector2 StartPosition;
+
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * ScrollSpeed * Time.deltaTime);
+    }
+}
