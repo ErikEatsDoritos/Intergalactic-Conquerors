35eb82acb2090c401f96d6cd9297b64d517c524f
ErikHatesHw <114367948+ErikEatsDoritos@users.noreply.github.com>
2023-11-02 00:00:17 -0400

    enemy properties work

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -2,82 +2,66 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 using UnityEngine;
-
+[System.Serializable]
 public class EnemyProperty : MonoBehaviour
 {
-    class EnemyStats
+    
+    private int _health;
+    
+    private int _dmg;
+    
+    private int _maxHealth;
+    [SerializeField()]
+    private int Tier;
+
+    public int GetHealth()
     {
-        [SerializeField()]
-        private int _health;
-        [SerializeField()]
-        private int _dmg;
-        [SerializeField()]
-        private int _maxHealth;
-        [SerializeField()]
-        private int Tier;
-        
-        
-        public EnemyStats(int tier)
-        {
-            this.Tier = tier;
-            if (this.Tier == 1)
-            {
-                _maxHealth = 3;
-                _dmg = 1;
-                
-            }
-            else if (this.Tier == 2)
-            {
-                _maxHealth = 4;
-                _dmg = 2;
-            }
-            else if (this.Tier == 3)
-            {
-                _maxHealth = 5;
-                _dmg = 3;
-            }
-            _health = _maxHealth;
-
-            
-        }
-        public void TakeDamage(int damage)
-        {
-            _health -= damage;
-        }
-
-        public void Death()
-        {
-            _health = 0;
-        }
-
-        public int GetDmg()
-        {
-            return _dmg;
-        }
-
-        
-
+        return _health;
     }
 
-
-    void Start()
+    public void SetTier(int tier)
     {
-        
+        this.Tier = tier;
+    }
 
-       
+    public int GetDmg()
+    {
+        return _dmg;
+    }
 
+    private void Start()
+    {
+       if(Tier == 1)
+        {
+            _maxHealth = 1;
+            _dmg = 1;
 
+        }
+       else if (Tier == 2)
+        {
+            _maxHealth = 1;
+            _dmg = 1;
 
+        }
+       else if (Tier == 3)
+        {
+            _maxHealth = 1;
+            _dmg = 1;
 
+        }
+        this._health = this._maxHealth;
     }
 
 
 
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
 
 }
 
@@ -85,7 +69,6 @@ public class EnemyProperty : MonoBehaviour
     
 
 
-    // Start is called before the first frame update
     
  
     
