9c518180e072262177ff11dd8bfba8b6ea02b774
ErikHatesHw <114367948+ErikEatsDoritos@users.noreply.github.com>
2023-11-02 14:26:06 -0400

    Made Spawning Better, and fixed up some bugs

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Windows;
+
 [System.Serializable]
 public class EnemyProperty : MonoBehaviour
 {
-    
+    [SerializeField()]
+    private GameObject Bullet;
+    private Rigidbody2D rb;
     private int _health;
-    
     private int _dmg;
-    
     private int _maxHealth;
     [SerializeField()]
     private int Tier;
@@ -31,6 +33,21 @@ public class EnemyProperty : MonoBehaviour
         return _dmg;
     }
 
+    public void TakeDmg(int dmg)
+    {
+        _health -= dmg;
+    }
+
+    public void Attack()
+    {
+        
+        Instantiate(Bullet, new Vector2(5, 4), Quaternion.identity);
+    }
+
+    
+    
+
+
     private void Start()
     {
        if(Tier == 1)
@@ -41,18 +58,46 @@ public class EnemyProperty : MonoBehaviour
         }
        else if (Tier == 2)
         {
-            _maxHealth = 1;
-            _dmg = 1;
+            _maxHealth = 2;
+            _dmg = 2;
 
         }
        else if (Tier == 3)
         {
-            _maxHealth = 1;
-            _dmg = 1;
+            _maxHealth = 3;
+            _dmg = 3;
 
         }
         this._health = this._maxHealth;
+        
+        
+
+        
+
+
+   
+
+        
     }
+
+    private void Update()
+    {
+        if (this._health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -0.5f);
+        
+
+
+
+
+
+    }
+
+
+
 
 
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,27 +1,70 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public class Spawner : MonoBehaviour
 {
-    public GameObject Tier3;
-    public GameObject Tier1;
-    public GameObject Tier2;
+    [SerializeField]
+    private GameObject Tier3;
+    [SerializeField]
+    private GameObject Tier1;
+    [SerializeField]
+    private GameObject Tier2;
+    private float _xAxis; 
+    private int _randomRoll; 
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnGameObjects()); // StartCorutine allows to call the delayed function 
+        _randomRoll = Random.Range(1, 4);
+        _xAxis = Random.Range(-10f, 10f);
+    }
+
+    public float Get_xAxis()
+    {
+        return _xAxis;
+    }
+
+    public void Set_xAxis(float value)
+    {
+        _xAxis = value;
+    }
+
+    public void FlipCoin(int value)
+    {
+        _randomRoll = value;
     }
 
     IEnumerator SpawnGameObjects() // IEnumerator allows for methods to be paused and delayed
     {
+       
+        
+
+
         while (true) 
         {
-            yield return new WaitForSeconds(Random.Range(1, 3)); // one yeild statement is needed for the coroutine to work
-            Instantiate(Tier1, new Vector2(Random.Range(-10f, 10f), 4), Quaternion.identity);
-            Instantiate(Tier2, new Vector2(Random.Range(-10f, 10f), 4), Quaternion.identity);
-            Instantiate(Tier3, new Vector2(Random.Range(-10f, 10f), 4), Quaternion.identity);
+            
+            yield return new WaitForSeconds(Random.Range(2, 5)); // one yeild statement is needed for the coroutine to work
+            
+            if (_randomRoll == 1) 
+            { 
+                Instantiate(Tier1, new Vector2(_xAxis, 4), Quaternion.identity);
+                Debug.Log("Rolled 1");
+            }
+            else if (_randomRoll == 2) 
+            { 
+                Instantiate(Tier2, new Vector2(_xAxis, 4), Quaternion.identity);
+                Debug.Log("Rolled 2");
+            }
+            else if (_randomRoll == 3) 
+            { 
+                Instantiate(Tier3, new Vector2(_xAxis, 4), Quaternion.identity);
+                Debug.Log("Rolled 3");
+            }
+            Set_xAxis(Random.Range(-10f, 10f));
+            FlipCoin(Random.Range(1, 4));
             Debug.Log("called method");
         }
     }
