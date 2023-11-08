72c98a801463c2e5f84a48d3b29a8bc3c84bd278
ErikHatesHw <114367948+ErikEatsDoritos@users.noreply.github.com>
2023-11-08 18:14:59 -0500

    fixed conflict

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -5,45 +5,32 @@ using UnityEngine;
 public class EnemyShip : MonoBehaviour
 {
     private int _damage;
-    
+    private int _health = 100;
     private GameObject _playerShip;
     private PlayerShip _playerInstance;
-    private EnemyProperty _enemyProperty;
-    public GameObject tier1;
+    public EnemyProperty _enemyProperty;
     
-    private EnemyProperty _enenmyStatsClass;
     
-
-
-
 
     private void Start()
     {
-        _enenmyStatsClass = tier1.GetComponent<EnemyProperty>();
-        _damage = _enenmyStatsClass.GetDmg();
 
-<<<<<<< HEAD
-        // gets values from enemy damage scrip
-=======
 
           // gets values from enemy damage script
         
->>>>>>> erik-branch
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+
     }
 
     public void Attack()
     {
         
-        _playerInstance.TakeDamage(_damage);
+        _playerInstance.TakeDamage(1);
         StartCoroutine(_playerInstance.ChangeColor());
-        Debug.Log(_damage);
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
